a8e05f88dcc20beed0333600c74b4c91fd71c9e4
Sonia Hakobian <sonyahakobyan97@gmail.com>
2017-10-19 00:07:37 +0400

    Add files via upload

---
+++ PhoneNumberValidator.js
@@ -0,0 +1,12 @@
+const Validator = require('./Validator');
+
+class ArmPhoneValidator extends Validator {
+  isArmPhone(phonenum) {
+    let reg =  /^(010\d{6}|091\d{6}|099\d{6}|041\d{6}|043\d{6}|012\d{6}|077\d{6}|095\d{6}|077\d{6}|093\d{6}|094\d{6}|098\d{6}|055\d{6}|[+]374\d{8}|[+]37410\d{6})$/g;
+    return reg.test(phonenum);
+  }
+}
+
+var a = new ArmPhoneValidator();
+
+console.log(a.isArmPhone('+37495769880'));
