b593ae2bb8f3c1c0d6d604d6be9efdc3e134ff87
Sonia Hakobian <sonyahakobyan97@gmail.com>
2017-10-19 00:08:56 +0400

    Add files via upload

---
+++ URLvalidator.js
@@ -0,0 +1,13 @@
+const Validator = require('./Validator');
+class URLValidator extends Validator
+{
+   isURlValid(userInput)
+   {
+      var res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+      if(res === null)
+          return false;
+          return true;
+   }
+}
+var a = new URLValidator();
+console.log(a.isURlValid('https://www.facebook.com'));
